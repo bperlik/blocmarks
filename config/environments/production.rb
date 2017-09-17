669f77c823a508f8e9b09001ece425aae81b43fa
Barbara Perlik <barbperlik@gmail.com>
2017-09-16 20:29:48 -0400

    fix production domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,6 +93,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # For mailer in production
-  config.action_mailer.default_url_options={ host: 'blocmarks-bp.herokuapp.com" }
+  config.action_mailer.default_url_options={ host: 'blocmarks-bp.herokuapp.com' }
 
 end
