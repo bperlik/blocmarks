0ba3a56363fc4afd9f23a94dbb9075a3603eafff
Barbara Perlik <barbperlik@gmail.com>
2017-09-18 00:52:09 -0400

    Faker out of development & confirm updates

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,9 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -30,11 +27,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
 
-  # configure Devise to send confirmation url to show in console
-  # copy url and paste in browser address
-  config.action_mailer.default_url_options={ host: 'localhost', port: 3000 }
-
-  # Print deprecation notices to the Rails logger.
+    # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
   # Raise an error on page load if there are pending migrations.
@@ -54,4 +47,9 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-end
+
+  # configure Devise to send confirmation url to show in console
+  # copy url and paste in browser address
+  config.action_mailer.default_url_options={ host: 'localhost', port: 3000 }
+
+  end
